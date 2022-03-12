55c7ed85854017735f10a904c08a4ee65c097eb0
Satyampatidar <63489154+satyampa@users.noreply.github.com>
2022-03-13 00:52:29 +0530

    Add files via upload

---
+++ CovidVaccinationProject(main)/CovidVaccinationProject/src/main/java/com/vaccination/app/CovidVaccinationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vaccination.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVaccinationProjectApplication {
+
+	public static void main(String[] args) {//server port-8084
+		SpringApplication.run(CovidVaccinationProjectApplication.class, args);
+	}
+
+}
---
+++ CovidVaccinationProject(main)/CovidVaccinationProject/src/main/java/com/vaccination/app/config/JPAConfiguration.java
@@ -0,0 +1,28 @@
+package com.vaccination.app.config;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+@Configuration
+public class JPAConfiguration {
+	@Bean
+    public EntityManagerFactory entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean emfb=new LocalContainerEntityManagerFactoryBean();
+        emfb.setPersistenceXmlLocation("classpath:META-INF/persistence.xml");
+        emfb.afterPropertiesSet();
+        return emfb.getObject();
+    }
+	@Bean
+    public DriverManagerDataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/ipl");
+        ds.setUsername("root");
+        ds.setPassword("Prakash@123");
+        return ds;
+    }
+}
